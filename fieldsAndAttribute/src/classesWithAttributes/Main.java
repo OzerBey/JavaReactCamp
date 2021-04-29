4962e2b078aed98949688898c8a9e7d62653f0ab
OzerBey <ysnozer.zer@gmail.com>
2021-04-29 16:04:31 +0300

    getter setter operations added and applied

--- fieldsAndAttribute/src/classesWithAttributes/Main.java
+++ fieldsAndAttribute/src/classesWithAttributes/Main.java
@@ -1,18 +1,17 @@
-package fieldsAndAttribute;
+package classesWithAttributes;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Product product = new Product();
-		product.id = 1;
-		product.name = "Mouse";
-		product.stockAmount = 3;
-		product.price = 250;
+		product.setId(1);
+		product.setName("Mouse");
+		product.setStockAmount(3);
+		product.setPrice(250);
 
 		ProductManager productManager = new ProductManager();
 		productManager.add(product);
-		
-
+		System.out.println(product.getPrice());
 	}
 
 }
--- fieldsAndAttribute/src/fieldsAndAttribute/ProductManager.java
+++
@@ -1,7 +0,0 @@
-package fieldsAndAttribute;
-
-public class ProductManager {
-	public void add(Product product) {
-		System.out.println(product.name + " added.");
-	}
-}
