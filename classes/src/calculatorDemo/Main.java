25fdf0cdf24d0d97e682409d3a5950c9b72997a0
OzerBey <ysnozer.zer@gmail.com>
2021-04-29 15:02:02 +0300

    calculator demo created and applied

---
+++ classes/src/calculatorDemo/Main.java
@@ -0,0 +1,12 @@
+package calculatorDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Process process = new Process();
+		System.out.println(process.minus(3, 4));
+		System.out.println(process.division(12, 24));
+
+	}
+
+}
