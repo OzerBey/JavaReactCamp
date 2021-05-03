be654c96b74c01b4f954cbb0da476e86005d9031
OzerBey <ysnozer.zer@gmail.com>
2021-05-03 19:52:14 +0300

    validationtest file udpated

--- MernisValidationTest/src/tr/gov/nvi/tckimlik/WS/Main.java
+++ MernisValidationTest/src/tr/gov/nvi/tckimlik/WS/Main.java
@@ -4,14 +4,16 @@ import java.rmi.RemoteException;
 
 public class Main {
 
+	// mernis check system url :
+	// https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx?WSDL
 	public static void main(String[] args) throws NumberFormatException, RemoteException {
-
+		boolean resultMernis;
 		KPSPublicSoapProxy kpsPublicSoapProxy = new KPSPublicSoapProxy();
-		boolean resultMernis = kpsPublicSoapProxy.TCKimlikNoDogrula(
-				Long.parseLong("25712344648"), //Tc
-				"FAHRİ", //name
-				"ÖZER", //surname
-				1996); //year of birth
+
+		resultMernis = kpsPublicSoapProxy.TCKimlikNoDogrula(Long.parseLong("01234567890"), // Tc
+				"YASİN", // name
+				"ÖZER", // surname
+				1998); // year of birth
 		System.err.println("Result of query: " + (resultMernis ? "Person is a real " : "Not a real person"));
 	}
 
