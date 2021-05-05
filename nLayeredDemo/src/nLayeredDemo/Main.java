0d1f3fdc42276e0d4a9692f0e2da91036649d96a
OzerBey <ysnozer.zer@gmail.com>
2021-05-06 00:08:10 +0300

    lecture 5 completed

---
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -0,0 +1,19 @@
+package nLayeredDemo;
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
+import nLayeredDemo.dataAccess.concretes.AbcProductDao;
+import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		// ToDo: it will refactoring with Spring IoC
+		ProductService productService = new ProductManager(new AbcProductDao(), new JLoggerManagerAdapter());
+		Product product = new Product(1, 2, "Apple", 15, 50);
+		productService.add(product);
+	}
+}
